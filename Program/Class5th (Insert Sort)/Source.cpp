16541cabdf6579bb99f4a27883f696d51390ac21
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2024-07-16 19:47:11 +0900

    (2024.07.16) 이분 탐색

--- Program/Class5th (Insert Sort)/Source.cpp
+++ Program/Class5th (Insert Sort)/Source.cpp
@@ -6,9 +6,57 @@ using namespace std;
 
 int main()
 {
+#pragma region 삽입 정렬
+    // 데이터를 하나씩 확인하면서 이미 정렬된 부분과 비교하여
+    // 자신의 위치를 찾아 삽입하는 방식으로 정렬되는 알고리즘입니다.
 
+    // 시작점은 n+1 [key] = n+1 
+    // sort 되기 위해 default key 랑 next key 를 비교삽입 
+    // && 
+    // list[len + 1] > key 만큼 비교
+
+    int list[SIZE] = { 13,8,97,1,36 };
+
+    int key = 0;
+    int j = 0;
+
+    for (int i = 1; i < SIZE; i++)
+    {
+        key = list[i];
+
+        for (j = i - 1; j >= 0 && list[j] > key; j--)
+        {
+            list[j + 1] = list[j];
+        }
+
+        list[j + 1] = key;
+    }
+
+    for (const int& element : list)
+    {
+        cout << element << " ";
+    }
+
+    // for (int i = 1; i < SIZE; i++)
+    // {
+    //     int key = list[i]; // default
+    //     int j = i - 1;
+    // 
+    //     while (j >= 0 && list[j] > key)
+    //     {
+    //         list[j + 1] = list[j];
+    //         j = j - 1;
+    //     }
+    //     list[j + 1] = key;
+    // }
+    // 
+    // for (const int& element : list)
+    // {
+    //     cout << element << " ";
+    // }
+
+#pragma endregion
 
 
     return 0;
 }
-
--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -2,62 +2,93 @@
 
 using namespace std;
 
-#define SIZE 5
+#define SIZE 8
+
+void BinarySearch(int list[], int key)
+{
+	int left = 0;
+	int right = SIZE - 1;
+
+	while (left <= right)
+	{
+		int pivot = (left + right) / 2;
+
+		if (list[pivot] == key)
+		{
+			cout << "key Found : " << list[pivot] << endl;
+			return;
+		}
+		else if (list[pivot] > key)
+		{
+			right = pivot - 1;
+		}
+		else
+		{
+			left = pivot + 1;
+		}
+	}
+
+	cout << "Not key Found" << endl;
+}
 
 int main()
 {
-#pragma region 삽입 정렬
-	// 데이터를 하나씩 확인하면서 이미 정렬된 부분과 비교하여
-	// 자신의 위치를 찾아 삽입하는 방식으로 정렬되는 알고리즘입니다.
+	int list[SIZE] = { 5, 7, 10, 15, 19, 20, 25 };
 
-    // 시작점은 n+1 [key] = n+1 
-    // sort 되기 위해 default key 랑 next key 를 비교삽입 
-    // && 
-    // list[len + 1] > key 만큼 비교
-
-    int list[SIZE] = { 13,8,97,1,36 };
-    
-    int key = 0;
-    int j = 0;
-
-    for (int i = 1; i < SIZE; i++)
-    {
-        key = list[i];
-
-        for (j = i - 1; j >= 0 && list[j] > key; j--)
-        {
-            list[j + 1] = list[j];
-        }
-
-        list[j + 1] = key;
-    }
-
-    for (const int & element : list)
-    {
-        cout << element << " ";
-    }
-
-    // for (int i = 1; i < SIZE; i++)
-    // {
-    //     int key = list[i]; // default
-    //     int j = i - 1;
-    // 
-    //     while (j >= 0 && list[j] > key)
-    //     {
-    //         list[j + 1] = list[j];
-    //         j = j - 1;
-    //     }
-    //     list[j + 1] = key;
-    // }
-    // 
-    // for (const int& element : list)
-    // {
-    //     cout << element << " ";
-    // }
-	
-#pragma endregion
-
+	BinarySearch(list, 20);
 
 	return 0;
 }
 
+//int binarysearch(int arr[], int size, int target) {
+//    int left = 0;
+//    int right = size - 1;
+//
+//    while (left <= right) {
+//        int middle = left + (right - left) / 2;  
+//
+//        if (arr[middle] == target) 
+//        {
+//            return middle;
+//        }
+//        else if (arr[middle] > target) 
+//        {
+//            right = middle - 1;
+//        }
+//        else 
+//        {
+//            left = middle + 1;
+//        }
+//    }
+//    return -1; 
+//}
+//
+//int main()
+//{
+//#pragma region 이진 탐색
+//	// 탐색 범위를 반으로 나누어 찾는 값을 포함하는 범위를
+//	// 좁혀나가는 방식으로 동작하는 탐색 알고리즘입니다.
+//
+//    int list[] = { 1, 5, 10, 12};
+//    int size = sizeof(list) / sizeof(list[0]);
+//    int target;
+//
+//    cout << "Element : ";
+//    cin >> target;
+//
+//    int result = binarysearch(list, size, target);
+//
+//    if (result != -1) 
+//    {
+//        cout << "index : " << result << endl;
+//    }
+//    else 
+//    {
+//        cout << "Element not found " << endl;
+//    }
+//
+//#pragma endregion
+//
+//
+//	return 0;
+//}
