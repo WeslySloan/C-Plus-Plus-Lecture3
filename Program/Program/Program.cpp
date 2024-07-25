5844b122f8c66b952e03ec6db301c36d02125d91
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2024-07-25 19:46:16 +0900

    (2024.07.25) 깊이 우선 탐색

--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -1,36 +1,101 @@
 ﻿#include <iostream>
+#include <vector>
 
 using namespace std;
 
-#define SIZE 11
+#define SIZE 8
 
-void hanoi(int n, char from, char by, char to)
+class Graph
 {
+private:
+	bool visited[SIZE];
+	vector<int> graph[SIZE];
+public:
+	Graph()
+	{
+		for (int i = 0; i < SIZE; i++)
+		{
+			visited[i] = false;
+		}
+	}
+	
+	void Insert(int vertex, int edge)
+	{
+		graph[vertex].push_back(edge);
+        graph[edge].push_back(vertex); 
+	}
 
+	void Search(int start)
+	{
+		// 1. 현재 노드를 방문한 것으로 표시합니다.
+		visited[start] = true;
 
+		// 2. 현재 노드를 출력합니다.
+		cout << start << " ";
 
-	return;
-}
+		// 3. 현재 노드와 연결된 다른 노드를 재귀적으로 방문합니다.
+		for (int i = 0; i < graph[start].size(); i++)
+		{
+			// 4. 현재 노드와 연결된 다음 노드를 가져옵니다.
+			int next = graph[start][i];
+
+			// 5. 다음 노드가 방문하지 않은 노드라면 재귀 함수를 호출합니다.
+			if (visited[next] == false)
+			{
+				Search(next);
+			}
+		}
+	}
+
+};
+
+// vector<int> graph[SIZE];
+// bool visited[SIZE] = { false };
+// void Insert(int vertex, int edge) 
+// {
+//     graph[vertex].push_back(edge);
+//     graph[edge].push_back(vertex); 
+// }
+// void Print() 
+// {
+// 	for (int i = 1; i < SIZE; i++)
+// 	{
+// 		for (int j = 0; j < graph[i].size(); j++)
+// 		{
+// 			cout << graph[i][j] << " ";
+// 		}
+// 		cout << endl;
+// 	}
+// }
+
 
 int main()
 {
-#pragma region 백트래킹
-	// 해를 찾아가는 도중에 지금의 경로가 해가 될 것 같지 않으면,
-	// 더 이상 깊이 들어가지 않고, 이전 단계로 다시 돌아가는 알고리즘입니다.
+#pragma region 깊이 우선 탐색
+	// root 노드에서 시작해서 다음 분기로 넘어가기 전에
+	// 해당 분기를 완벽하게 탐색하는 탐색 알고리즘입니다.
 
-	// 백트래킹의 유망성 판단
-	// 해가 될 만한지 판단한 후에 유망하지 않다고 결정되면,
-	// 그 노드의 이전 노드로 돌아가 다음 자식 노드로 이동합니다.
+	// 1. 시작 노드를 ㅅ스택에 넣고 방문 처리를 합니다.
 
-	// 해가 될 만한 가능성이 있으면 유망하다. (Promising)
-	// 유망하지 않은 노드에 가지 않는 것	  (Pruning)
+	// 2. 스택의 최상단 노드에 방문하지 않은 인접 노드가 있으면
+	//    그 노드를 스택에 넣고 방문 처리합니다.
+
+	// 3. 방문하지 않은 인접 노드가 없으면 스택에서 최상단 노드를 꺼
+
+	// 4. 더 이상 2번의 과정을 수행할 수 없을 때까지 반복합니다.
 
 
 
 #pragma endregion
 
+	Graph graph;
 
-
+	graph.Insert(1, 2);
+	graph.Insert(1, 3);
+	graph.Insert(2, 4);
+	graph.Insert(2, 5);
+	graph.Insert(3, 6);
+	graph.Insert(3, 7);
 
 	return 0;
 }
