8f8aeb126a0eea05b0e6b2deb1f375ea3ed06815
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2024-07-26 19:46:48 +0900

    (2024.07.26) 너비 우선 탐색

--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -1,15 +1,21 @@
 ﻿#include <iostream>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
 #define SIZE 8
 
+vector<int> graph[SIZE];
+bool visited[SIZE] = { false };
+
 class Graph
 {
 private:
-	bool visited[SIZE];
+	int degree[SIZE];
+	queue<int> queue;
 	vector<int> graph[SIZE];
+
 public:
 	Graph()
 	{
@@ -18,84 +24,50 @@ public:
 			visited[i] = false;
 		}
 	}
-	
+
 	void Insert(int vertex, int edge)
 	{
 		graph[vertex].push_back(edge);
-        graph[edge].push_back(vertex); 
-	}
-
-	void Search(int start)
-	{
-		// 1. 현재 노드를 방문한 것으로 표시합니다.
-		visited[start] = true;
-
-		// 2. 현재 노드를 출력합니다.
-		cout << start << " ";
-
-		// 3. 현재 노드와 연결된 다른 노드를 재귀적으로 방문합니다.
-		for (int i = 0; i < graph[start].size(); i++)
-		{
-			// 4. 현재 노드와 연결된 다음 노드를 가져옵니다.
-			int next = graph[start][i];
-
-			// 5. 다음 노드가 방문하지 않은 노드라면 재귀 함수를 호출합니다.
-			if (visited[next] == false)
-			{
-				Search(next);
-			}
-		}
+		graph[edge].push_back(vertex);
 	}
 
 };
 
-// vector<int> graph[SIZE];
-// bool visited[SIZE] = { false };
-// void Insert(int vertex, int edge) 
-// {
-//     graph[vertex].push_back(edge);
-//     graph[edge].push_back(vertex); 
-// }
-// void Print() 
-// {
-// 	for (int i = 1; i < SIZE; i++)
-// 	{
-// 		for (int j = 0; j < graph[i].size(); j++)
-// 		{
-// 			cout << graph[i][j] << " ";
-// 		}
-// 		cout << endl;
-// 	}
-// }
-
 
 int main()
 {
-#pragma region 깊이 우선 탐색
-	// root 노드에서 시작해서 다음 분기로 넘어가기 전에
-	// 해당 분기를 완벽하게 탐색하는 탐색 알고리즘입니다.
+#pragma region 위상 정렬
+	// 방향 그래프에 존재하는 각 정점들의 선행 순서를 지키며,
+	// 모든 정점을 차례대로 진행하는 알고리즘입니다.
 
-	// 1. 시작 노드를 ㅅ스택에 넣고 방문 처리를 합니다.
+	// 사이클이 발생하는 경우 위상 정렬을 수행할 수 없습니다.
 
-	// 2. 스택의 최상단 노드에 방문하지 않은 인접 노드가 있으면
-	//    그 노드를 스택에 넣고 방문 처리합니다.
+	// DAG(Directed Acyclic Graph) : 사이클이 존재하지 않는 그래프
 
-	// 3. 방문하지 않은 인접 노드가 없으면 스택에서 최상단 노드를 꺼
+	// 시간 복잡도 : O(V + E)
 
-	// 4. 더 이상 2번의 과정을 수행할 수 없을 때까지 반복합니다.
+	// 위상 정렬하는 방법
+	
+	// 1. 진입 차수가 0인 정점을 Queue에 삽입합니다.
 
+	// 2. Queue에서 원소를 꺼내 연결된 모든 간선을 제거합니다.
+
+	// 3. 간선 제거 이후에 진입 차수가 0이 된 정점을 Queue에 삽입합니다.
+
+	// 4. Queue가 비어있을 때까지 2번 ~ 3번을 반복 수행합니다.
 
 
 #pragma endregion
 
 	Graph graph;
 
-	graph.Insert(1, 2);
-	graph.Insert(1, 3);
-	graph.Insert(2, 4);
-	graph.Insert(2, 5);
-	graph.Insert(3, 6);
-	graph.Insert(3, 7);
+	graph.Insert(1, 1);
+	graph.Insert(2, 2);
+	graph.Insert(3, 3);
+	graph.Insert(4, 5);
+	graph.Insert(5, 6);
+
+
 
 	return 0;
 }
