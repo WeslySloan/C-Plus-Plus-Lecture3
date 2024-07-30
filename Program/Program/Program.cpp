dd27738855869ee6b0ce94688dae1ff7997938f3
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2024-07-30 19:47:40 +0900

    (2024.07.30) 최소 신장 트리

--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -1,65 +1,114 @@
 ﻿#include <iostream>
-#include <queue>
 #include <vector>
+#include <algorithm>
 
 #define SIZE 7
 
 using namespace std;
 
+//class Graph
+//{
+//private:
+//	class Edge
+//	{
+//	public:
+//		int x;
+//		int y;
+//		int distance;
+//
+//		Edge(int x, int y, int distance) : x(x), y(y), distance(distance) {}
+//	};
+//
+//	vector<Edge> graph;
+//
+//public:
+//	Graph()
+//	{
+//
+//	}
+//
+//	void Insert(int x, int y, int distance)
+//	{
+//		graph.push_back(Edge(x, y, distance));
+//	}
+//
+//
+//};
+
 class Graph
 {
 private:
-	int parent[SIZE];
-
+    bool visited[SIZE];
+    vector<int> graph[SIZE];
 public:
-	Graph()
-	{
-		for (int i = 0; i < SIZE; i++)
-		{
-			parent[i] = 0;
-		}
-	}
+    Graph()
+    {
+        for (int i = 0; i < SIZE; i++)
+        {
+            visited[i] = false;
+        }
+    }
 
-	int Find(int x)
-	{
-		if (parent[x] == x)
-		{
-			return x;
-		}
-		else
-		{
-			return parent[x] = Find(parent[x]);
-		}
-	}
+    void Insert(int vertex, int edge)
+    {
+        graph[vertex].push_back(edge);
+        graph[edge].push_back(vertex);
+    }
 
-	void Union(int x, int y)
-	{
+    void Search(int start)
+    {
+        // 1. 현재 노드를 방문한 것으로 표시합니다.
+        visited[start] = true;
 
-	}
+        // 2. 현재 노드를 출력합니다.
+        cout << start << " ";
 
-	bool Same(int x, int y)
-	{
+        // 3. 현재 노드와 연결된 다른 노드를 재귀적으로 방문합니다.
+        for (int i = 0; i < graph[start].size(); i++)
+        {
+            // 4. 현재 노드와 연결된 다음 노드를 가져옵니다.
+            int next = graph[start][i];
 
-	}
-
+            // 5. 다음 노드가 방문하지 않은 노드라면 재귀 함수를 호출합니다.
+            if (visited[next] == false)
+            {
+                Search(next);
+            }
+        }
+    }
 };
 
-
-
-int main()
+int main ()
 {
-#pragma region 유니온 파인드
-	// 여러 노드가 존재할 때 어떤 노드가 다른 노드와 연결되어
-	// 있는지 확인하는 알고리즘입니다.
+#pragma region 신장 트리
+	// 그래프의 모든 정점을 포함하면서 사이클이 존재하지 않는
+	// 부분 그래프로, 그래프의 모든 정점을 최소 비용으로 연결하는 트리입니다.
 
-	// 유니온 파인드의 시간 복잡도
-	// O(MlogN) : M은 연산의 개수, N은 노드의 개수
-	// M이 N²에 가까울 때는 O(N²logN)이 됩니다.
+	// 그래프의 정점의 수가 n개일 때, 간선의 수는 n-1개 입니다.
+
+	// 최소 비용 신장 트리
+	// 그래프의 간선들의 가중치 합이 최소인 신장 트리
+
+	Graph graph;
+
+
+	graph.Insert(1, 7, 10);
+	graph.Insert(1, 4, 28);
+	graph.Insert(1, 2, 66);
+	graph.Insert(1, 5, 19);
+
+	graph.Insert(2, 4, 20);
+	graph.Insert(2, 5, 60);
+
+	graph.Insert(3, 5, 21);
+	graph.Insert(3, 6, 35);
+
+	graph.Insert(4, 7, 15);
+	graph.Insert(5, 6, 40);
 
 
 #pragma endregion
 
-	Graph graph;
 
 
 	return 0;
