dcbf7be1d6c097306dd4ce13b7179e5f1e83843e
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2024-07-31 19:46:20 +0900

    (2024.07.31) 누적합

---
+++ Program/Class20th (Minimum Spanning Tree)/Source.cpp
@@ -0,0 +1,212 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define SIZE 8
+
+using namespace std;
+
+//class Graph
+//{
+//private:
+//	class Edge
+//	{
+//	public:
+//		int x;
+//		int y;
+//		int distance;
+//
+//		Edge(int x, int y, int distance) : x(x), y(y), distance(distance) {}
+//	};
+//
+//	vector<Edge> graph;
+//
+//public:
+//	Graph()
+//	{
+//
+//	}
+//
+//	void Insert(int x, int y, int distance)
+//	{
+//		graph.push_back(Edge(x, y, distance));
+//	}
+//
+//
+//};
+
+class Graph
+{
+private:
+    class Edge
+    {
+    private:
+        int x;
+        int y;
+        int distance;
+
+    public:
+        Edge(int x, int y, int distance)
+        {
+            this->x = x;
+            this->y = y;
+            this->distance = distance;
+        }
+
+        int& X()
+        {
+            return x;
+        }
+
+        int& Y()
+        {
+            return y;
+        }
+
+        int& Distance()
+        {
+            return distance;
+        }
+
+        bool operator < (const Edge& edge)
+        {
+            return this->distance < edge.distance;
+        }
+    };
+
+    vector<Edge> graph;
+
+    int cost;
+    int parent[SIZE];
+public:
+    Graph()
+    {
+
+    }
+
+    void Insert(int x, int y, int distance)
+    {
+        graph.push_back(Edge(x, y, distance));
+    }
+
+    int Find(int x)
+    {
+        if (x == parent[x])
+        {
+            return x;
+        }
+        else
+        {
+            return parent[x] = Find(parent[x]);
+        }
+    }
+
+    void Union(int x, int y)
+    {
+        x = Find(x);
+        y = Find(y);
+
+        if (x < y)
+        {
+            parent[y] = x;
+        }
+        else
+        {
+            parent[x] = y;
+        }
+    }
+
+    bool Same(int x, int y)
+    {
+        return Find(x) == Find(y);
+    }
+
+
+
+    void Kruskal()
+    {
+        sort(graph.begin(), graph.end());
+
+        for (int i = 0; i < graph.size(); i++)
+        {
+            if (Same(graph[i].X(), graph[i].Y()) == false)
+            {
+                cost += graph[i].Distance();
+
+                Union(graph[i].X(), graph[i].Y());
+            }
+        }
+
+        cout << "Graph 최소 비용 : " << cost << endl;
+    }
+
+    //void Kruskal() 
+    //{
+    //    sort(graph.begin(), graph.end());  // 연산자 오버로딩 덕분에 정렬이 가능해짐
+    //    int parent[SIZE];
+    //    int rank[SIZE] = { 0 };
+    //    for (int i = 1; i < SIZE; i++) 
+    //    {
+    //        parent[i] = i;
+    //    }
+    //    int mst_weight = 0;
+    //    vector<Edge> mst_edges;
+    //    for (auto& edge : graph) 
+    //    {
+    //        int x = edge.X();
+    //        int y = edge.Y();
+    //        int dist = edge.Distance();
+    //        if (Find(parent, x) != Find(parent, y)) 
+    //        {
+    //            mst_weight += dist;
+    //            mst_edges.push_back(edge);
+    //            Union(parent, rank, x, y);
+    //        }
+    //    }
+    //    cout << mst_weight << endl;
+    //    for (auto& edge : mst_edges) 
+    //    {
+    //        cout << "X : " << edge.X() << " / " << "Y : " << edge.Y() << " / " << "Distaqnce : " << edge.Distance() << endl;
+    //    }
+    //}
+
+
+
+
+};
+
+int main()
+{
+#pragma region 신장 트리
+    // 그래프의 모든 정점을 포함하면서 사이클이 존재하지 않는
+    // 부분 그래프로, 그래프의 모든 정점을 최소 비용으로 연결하는 트리입니다.
+
+    // 그래프의 정점의 수가 n개일 때, 간선의 수는 n-1개 입니다.
+
+    // 최소 비용 신장 트리
+    // 그래프의 간선들의 가중치 합이 최소인 신장 트리
+
+    Graph graph;
+
+
+    graph.Insert(1, 7, 10);
+    graph.Insert(1, 4, 28);
+    graph.Insert(1, 2, 66);
+    graph.Insert(1, 5, 19);
+
+    graph.Insert(2, 4, 25);
+    graph.Insert(2, 5, 60);
+
+    graph.Insert(3, 5, 21);
+    graph.Insert(3, 6, 35);
+
+    graph.Insert(4, 7, 15);
+    graph.Insert(5, 6, 40);
+
+    graph.Kruskal();
+#pragma endregion
+
+    // 10 + 15 + 19 + 21 + 25 + 35
+
+    return 0;
+}
--- Program/Program/Program.cpp
+++ Program/Program/Program.cpp
@@ -1,114 +1,29 @@
 ﻿#include <iostream>
-#include <vector>
-#include <algorithm>
-
-#define SIZE 7
 
 using namespace std;
 
-//class Graph
-//{
-//private:
-//	class Edge
-//	{
-//	public:
-//		int x;
-//		int y;
-//		int distance;
-//
-//		Edge(int x, int y, int distance) : x(x), y(y), distance(distance) {}
-//	};
-//
-//	vector<Edge> graph;
-//
-//public:
-//	Graph()
-//	{
-//
-//	}
-//
-//	void Insert(int x, int y, int distance)
-//	{
-//		graph.push_back(Edge(x, y, distance));
-//	}
-//
-//
-//};
-
-class Graph
-{
-private:
-    bool visited[SIZE];
-    vector<int> graph[SIZE];
-public:
-    Graph()
-    {
-        for (int i = 0; i < SIZE; i++)
-        {
-            visited[i] = false;
-        }
-    }
-
-    void Insert(int vertex, int edge)
-    {
-        graph[vertex].push_back(edge);
-        graph[edge].push_back(vertex);
-    }
-
-    void Search(int start)
-    {
-        // 1. 현재 노드를 방문한 것으로 표시합니다.
-        visited[start] = true;
-
-        // 2. 현재 노드를 출력합니다.
-        cout << start << " ";
-
-        // 3. 현재 노드와 연결된 다른 노드를 재귀적으로 방문합니다.
-        for (int i = 0; i < graph[start].size(); i++)
-        {
-            // 4. 현재 노드와 연결된 다음 노드를 가져옵니다.
-            int next = graph[start][i];
-
-            // 5. 다음 노드가 방문하지 않은 노드라면 재귀 함수를 호출합니다.
-            if (visited[next] == false)
-            {
-                Search(next);
-            }
-        }
-    }
-};
-
 int main ()
 {
-#pragma region 신장 트리
-	// 그래프의 모든 정점을 포함하면서 사이클이 존재하지 않는
-	// 부분 그래프로, 그래프의 모든 정점을 최소 비용으로 연결하는 트리입니다.
-
-	// 그래프의 정점의 수가 n개일 때, 간선의 수는 n-1개 입니다.
-
-	// 최소 비용 신장 트리
-	// 그래프의 간선들의 가중치 합이 최소인 신장 트리
-
-	Graph graph;
+#pragma region 누적합
+	// 특정한 배열이 있을 때, 배열까지의 합을 나타내는 것입니다.
 
 
-	graph.Insert(1, 7, 10);
-	graph.Insert(1, 4, 28);
-	graph.Insert(1, 2, 66);
-	graph.Insert(1, 5, 19);
+	int arr[5] = { 1,2,3,4,5 };
+	int result[5];
 
-	graph.Insert(2, 4, 20);
-	graph.Insert(2, 5, 60);
+	result[0] = arr[0];
 
-	graph.Insert(3, 5, 21);
-	graph.Insert(3, 6, 35);
+	for (int i = 1; i < 5; i++)
+	{
+		result[i] = result[i - 1] + arr[i];
+	}
 
-	graph.Insert(4, 7, 15);
-	graph.Insert(5, 6, 40);
-
+	for (int i = 0; i < 5; i++)
+	{
+		cout << result[i] << " ";
+	}
 
 #pragma endregion
-
 
 
 	return 0;
